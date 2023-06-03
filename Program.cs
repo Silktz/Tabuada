60172c5036b3c86e500f8cd5d72af30649709e3a
Silktz <caua.franco3@etec.sp.gov.br>
2023-06-02 22:23:34 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//By Silktz 
+Console.WriteLine("TABUADA DE MULTIPLICAÇÃO");
+
+int resultado;
+int multiplicador = 0;
+
+Console.WriteLine("Digite um número inteiro para mostragem de tabuada...");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+while (multiplicador <= 10)
+{
+resultado = numero*multiplicador;
+    Console.WriteLine($"{numero} x {multiplicador} = {resultado} ");
+    multiplicador += 1;
+}
+
+
+
